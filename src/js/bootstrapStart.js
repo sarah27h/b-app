1c839527be054c5c901ee5087d77dcde6c589104
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2021-04-16 00:03:07 +0200

    Feat: create header & hero section

--- src/js/bootstrapStart.js
+++ src/js/bootstrapStart.js
@@ -21,7 +21,7 @@ import '../../node_modules/popper.js/dist/popper.min.js';
 import '../../node_modules/bootstrap/js/dist/util.js';
 import '../../node_modules/bootstrap/js/dist/modal.js';
 import '../../node_modules/bootstrap/js/dist/dropdown.js';
-// import '../../node_modules/bootstrap/js/dist/collapse.js';
+import '../../node_modules/bootstrap/js/dist/collapse.js';
 // import "../../node_modules/bootstrap/js/dist/tooltip.js";
 // import "../../node_modules/bootstrap/js/dist/popover.js";
 
